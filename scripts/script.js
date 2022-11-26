7bc68ffec0833c6c9f5c4988d0aa684cb58fab8c
KuraiPiero <54311200+KuraiPiero@users.noreply.github.com>
2022-11-26 11:08:04 -0500

    Shadows were added on styles.css On script were added the new slides to change when another section selected

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,36 @@
-document.querySelector("title").innerHTML = "Alejo Martinez";
+document.querySelector('title').innerHTML = 'Alejo Martinez'
 
+const sections = Array.from(document.querySelectorAll('.curriculum'))
+const slide = Array.from(document.querySelectorAll('.header__list-item--slide'))
 const headerButtons = Array.from(
-  document.querySelectorAll(".header__list-item")
-);
-const sections = Array.from(document.querySelectorAll(".curriculum"));
-const buttonSelection = headerButtons.forEach((button) => {
-  button.addEventListener("click", function () {
-    const buttonSiblings = headerButtons.filter(
-      (siblingButton) => siblingButton !== button
-    );
-    buttonSiblings.forEach(function (otherButton) {
-      otherButton.classList.remove("active");
-    });
-    this.classList.add("active");
-
-    if (this.innerText == "Summary") {
-      sections[0].open = false;
-      sections[0].style.display = "none";
-      sections[1].open = true;
-      sections[1].style.display = "grid";
-    } else {
-      sections[0].open = true;
-      sections[0].style.display = "grid";
-      sections[1].open = false;
-      sections[1].style.display = "none";
-    }
-  });
-});
+   document.querySelectorAll('.header__list-item')
+)
 const progressBarFill = Array.from(
-  document.querySelectorAll(".skill__progress-bar--fill")
-);
+   document.querySelectorAll('.skill__progress-bar--fill')
+)
+
+const buttonSelection = headerButtons.forEach((button) => {
+   button.addEventListener('click', function () {
+      let otherButton = headerButtons.filter(
+         (sideButton) => sideButton !== button
+      )
+      otherButton.forEach(function (sideButton) {
+         sideButton.classList.remove('active')
+      }),
+         this.classList.add('active')
+      'Summary' == this.innerText
+         ? ((sections[0].style.display = 'none'),
+           (sections[1].style.display = 'grid'),
+           slide[0].classList.remove('active'),
+           slide[1].classList.add('active'))
+         : ((sections[0].style.display = 'grid'),
+           (sections[1].style.display = 'none'),
+           slide[1].classList.remove('active'),
+           slide[0].classList.add('active'))
+   })
+})
 
 progressBarFill.forEach((bar) => {
-  let width = bar.innerHTML;
-  bar.style.color = "transparent";
-
-  bar.style.width = `${width}%`;
-});
+   let width = bar.innerHTML
+   ;(bar.style.color = 'transparent'), (bar.style.width = `${width}%`)
+})
