a0c0d3ab5b75f44e89489559e093ba73b8256cb6
KuraiPiero <54311200+KuraiPiero@users.noreply.github.com>
2022-11-28 20:23:23 -0500

    new classes were create to get user data

---
+++ scripts/data.js
@@ -0,0 +1,33 @@
+class personalInfo {
+   constructor(name, title, skills, languages, description) {
+      this.name = name
+      this.title = title
+      this.skills = skills
+      this.languages = languages
+      this.description = description
+   }
+}
+
+class professionalInfo {
+   constructor(
+      title,
+      companyName,
+      intensity,
+      initialDate,
+      finalDate,
+      place,
+      description,
+      activities,
+      aditionalNotes
+   ) {
+      this.title = title
+      this.companyName = companyName
+      this.intensity = intensity
+      this.initialDate = initialDate
+      this.finalDate = finalDate
+      this.place = place
+      this.description = description
+      this.activities = activities
+      this.aditionalNotes = aditionalNotes
+   }
+}
