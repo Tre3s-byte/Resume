3ad07cd6538905badf947490240d65082f96d0a6
KuraiPiero <54311200+KuraiPiero@users.noreply.github.com>
2022-12-01 20:45:59 -0500

    To update section & buttons, there was created an anonymous function; started to create string literals to create hover effects affected by the current mouse pointer position

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,25 @@
 document.querySelector('title').innerHTML = 'Alejo Martinez'
-
-const sections = Array.from(document.querySelectorAll('.curriculum'))
-const slide = Array.from(document.querySelectorAll('.header__list-item--slide'))
-const headerButtons = Array.from(
-   document.querySelectorAll('.header__list-item')
-)
-
-const buttonSelection = headerButtons.forEach((button) => {
-   button.addEventListener('click', function () {
-      let otherButton = headerButtons.filter(
-         (sideButton) => sideButton !== button
-      )
-      otherButton.forEach(function (sideButton) {
-         sideButton.classList.remove('active')
-      }),
-         this.classList.add('active')
-      'Summary' == this.innerText
-         ? ((sections[0].style.display = 'none'),
-           (sections[1].style.display = 'grid'),
-           slide[0].classList.remove('active'),
-           slide[1].classList.add('active'))
-         : ((sections[0].style.display = 'grid'),
-           (sections[1].style.display = 'none'),
-           slide[1].classList.remove('active'),
-           slide[0].classList.add('active'))
+;(function () {
+   const sections = Array.from(document.querySelectorAll('.curriculum'))
+   const headerButtons = Array.from(
+      document.querySelectorAll('.header__list-item')
+   )
+   const buttonSelection = headerButtons.forEach((button) => {
+      button.addEventListener('click', function () {
+         let otherButton = headerButtons.filter(
+            (sideButton) => sideButton !== button
+         )
+         otherButton.forEach(function (sideButton) {
+            sideButton.classList.remove('active')
+         }),
+            this.classList.add('active')
+         'Summary' == this.innerText
+            ? ((sections[0].style.display = 'none'),
+              (sections[1].style.display = 'grid'))
+            : ((sections[0].style.display = 'grid'),
+              (sections[1].style.display = 'none'))
+      })
    })
-})
-
-function createBoxes() {
    let tagTarget = Array.from(document.querySelectorAll('.skill__progress-bar'))
 
    let percentanges = [100, 80, 80, 60, 60, 60]
@@ -42,5 +34,37 @@ function createBoxes() {
       progressBar.height = '1em'
       progressBar.backgroundColor = '#ffc500'
    })
-}
-createBoxes()
+})()
+
+const cssTemplateString = [
+   `.header__list-item:hover {
+   color: var(--golden-poppy);
+   transform: scale(1.02) translate(-2px, 0px);
+}`,
+   `.header__list-item::before {
+   transition: opacity 0.3s, border 0.3s;
+}`,
+   `.header__list-item::after {
+   content: '';
+   position: absolute;
+   top: 0;
+   left: 0;
+   width: 200px;
+   height: 200px;
+   background-color: var(--tabaco);
+   border-color: transparent;
+   border-radius: 50%;
+   transform: translate(-50px, -70px) scale(0.1);
+   opacity: 0;
+   z-index: -1;
+   transition: transform 0.3s, opacity 0.3s, background-color 0.3s;
+}`,
+   `.header__list-item:hover::after {
+   opacity: 1;
+   transform-origin: 100px 100px;
+   transform: scale(1.2) translate(-10px, -70px);
+}`,
+]
+const styleTag = document.createElement('style')
+styleTag.innerHTML = cssTemplateString
+document.head.insertAdjacentElement('beforeend', styleTag)
